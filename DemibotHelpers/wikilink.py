a9266333f05a80a71ed8f08986baa88484bf2503
demize <demize95@demize95.com>
2015-01-12 21:11:29 -0500

    Minor fix to remove whitespace from wikilinks

--- DemibotHelpers/wikilink.py
+++ DemibotHelpers/wikilink.py
@@ -9,22 +9,22 @@ class Wikilink(object):
     def getLinkText(self):
         if "|" not in self.wikilink:
             if self.wikilink[2:3] == ":":
-                return self.wikilink[3:-2]
-            return self.wikilink[2:-2]
+                return self.wikilink[3:-2].strip()
+            return self.wikilink[2:-2].strip()
         text = self.wikilink.split("|", 1)[1][:-2]
         if text == "":
             text = self.wikilink.split("|", 1)[0].split(":", 1)[1]
             if text[:1] == ":":
-                return text[1:]
-        return text
+                return text[1:].strip()
+        return text.strip()
 
     def getLinkTarget(self):
         if "|" not in self.wikilink:
             return self.getLinkText()
         target = self.wikilink.split("|", 1)[0][2:]
         if target[:1] == ":":
-            return target[1:]
-        return target
+            return target[1:].strip()
+        return target.strip()
 
 class BadWikilinkException(Exception):
     pass
