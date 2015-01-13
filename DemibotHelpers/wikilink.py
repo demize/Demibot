cd4fb1cb0550c5f7acd2b38dbe1dc7863a9087f2
demize <demize95@demize95.com>
2015-01-12 21:01:20 -0500

    Improvements to wikilink helper class

--- DemibotHelpers/wikilink.py
+++ DemibotHelpers/wikilink.py
@@ -8,17 +8,23 @@ class Wikilink(object):
 
     def getLinkText(self):
         if "|" not in self.wikilink:
+            if self.wikilink[2:3] == ":":
+                return self.wikilink[3:-2]
             return self.wikilink[2:-2]
         text = self.wikilink.split("|", 1)[1][:-2]
         if text == "":
-            return self.wikilink.split("|", 1)[0].split(":", 1)[1]
+            text = self.wikilink.split("|", 1)[0].split(":", 1)[1]
+            if text[:1] == ":":
+                return text[1:]
         return text
 
     def getLinkTarget(self):
         if "|" not in self.wikilink:
-            return self.wikilink[2:-2]
-        else:
-            return self.wikilink.split("|")[0][2:]
+            return self.getLinkText()
+        target = self.wikilink.split("|", 1)[0][2:]
+        if target[:1] == ":":
+            return target[1:]
+        return target
 
 class BadWikilinkException(Exception):
     pass
